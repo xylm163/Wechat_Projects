e9967bdc5c044e2b6d7b877cbcb96928cc1e67bd
xylm163 <xylm163@163.com>
2025-02-27 21:09:32 +0800

    initial commit

---
+++ minisoft-1/pages/index/index.js
@@ -0,0 +1,14 @@
+// index.js
+const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+
+Page({
+  data: {
+      wording: "world"
+  },
+
+  onClick: function() {
+      this.setData({
+          wording: 'girl'
+      })
+  }
+})
